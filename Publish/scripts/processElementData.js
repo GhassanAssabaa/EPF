4bc410ba05961b4fabfbe705be6a9226eddebfde
GhassanAssabaa <56797081+GhassanAssabaa@users.noreply.github.com>
2020-11-07 22:25:48 +0100

    commit

---
+++ Publish/scripts/processElementData.js
@@ -0,0 +1,54 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.elementUrls["_ulb40Oz3EemVzN-LgHwcjw"] = ["Model developing ","ProcessTestBI/deliveryprocesses/model_developing__75476916.html"];
+contentPage.processPage.elementUrls["_5-LdQOz4EemVzN-LgHwcjw"] = ["Deployment","ProcessTestBI/deliveryprocesses/Deployment_12E7CEDD.html"];
+contentPage.processPage.elementUrls["_UfluIezzEemVzN-LgHwcjw"] = ["Data ingest &amp; understanding","ProcessTestBI/deliveryprocesses/Data ingest understanding_AA42270B.html"];
+contentPage.processPage.elementUrls["_rC_Fwez3EemVzN-LgHwcjw"] = ["Modeling","ProcessTestBI/deliveryprocesses/Modeling_F5B0EB4.html"];
+contentPage.processPage.elementUrls["_cbS8QOz6EemVzN-LgHwcjw"] = ["Project Manager","ProcessTestBI/deliveryprocesses/project_manager_8A750F13.html"];
+contentPage.processPage.elementUrls["_43D90OzwEemVzN-LgHwcjw"] = ["Project Manager","ProcessTestBI/deliveryprocesses/project_manager_3CE95FE5.html"];
+contentPage.processPage.elementUrls["_YAIYgOz6EemVzN-LgHwcjw"] = ["Transition to production support","ProcessTestBI/deliveryprocesses/transition_to_production_support_4B1C811.html"];
+contentPage.processPage.elementUrls["_3jI30OzwEemVzN-LgHwcjw"] = ["Create template repository","ProcessTestBI/deliveryprocesses/create_template_repository_1F6B4952.html"];
+contentPage.processPage.elementUrls["_hYhvcezzEemVzN-LgHwcjw"] = ["Solution architecture diagram","ProcessTestBI/deliveryprocesses/solution_architecture_diagram_285E88B6.html"];
+contentPage.processPage.elementUrls["_FnRAQez5EemVzN-LgHwcjw"] = ["Dashboards","ProcessTestBI/deliveryprocesses/dashboards_CC93B05B.html"];
+contentPage.processPage.elementUrls["_HRH6AOz5EemVzN-LgHwcjw"] = ["Deploy pipeline","ProcessTestBI/deliveryprocesses/deploy_pipeline_5E888276.html"];
+contentPage.processPage.elementUrls["_b2K6IezzEemVzN-LgHwcjw"] = ["Data scientist","ProcessTestBI/deliveryprocesses/data_scientist_7730122C.html"];
+contentPage.processPage.elementUrls["_d_8DAOzzEemVzN-LgHwcjw"] = ["Data ingest &amp; explore","ProcessTestBI/deliveryprocesses/data_ingest__explore_B644F98A.html"];
+contentPage.processPage.elementUrls["_aiWh0OzzEemVzN-LgHwcjw"] = ["Chief project","ProcessTestBI/deliveryprocesses/chief_project_8B8C114D.html"];
+contentPage.processPage.elementUrls["_k1YYMOzwEemVzN-LgHwcjw"] = ["Business Understanding","ProcessTestBI/deliveryprocesses/Business Understanding_48BF87A.html"];
+contentPage.processPage.elementUrls["_3jJe4OzwEemVzN-LgHwcjw"] = ["Chief project","ProcessTestBI/deliveryprocesses/chief_project_449BBFDF.html"];
+contentPage.processPage.elementUrls["_ELTYkOz5EemVzN-LgHwcjw"] = ["Deploy scoring process","ProcessTestBI/deliveryprocesses/deploy_scoring_process_B9321480.html"];
+contentPage.processPage.elementUrls["_tkVoMOz3EemVzN-LgHwcjw"] = ["Feature engineering","ProcessTestBI/deliveryprocesses/feature_engineering_B15DD180.html"];
+contentPage.processPage.elementUrls["_UsBvAOzwEemVzN-LgHwcjw"] = ["DataProjects_Process","ProcessTestBI/deliveryprocesses/DataProjects_Process_FA6970F2.html"];
+contentPage.processPage.elementUrls["_ELT_oOz5EemVzN-LgHwcjw"] = ["Data scientist","ProcessTestBI/deliveryprocesses/data_scientist_9D77E782.html"];
+contentPage.processPage.elementUrls["_cbSVMOz6EemVzN-LgHwcjw"] = ["Finalize documentation","ProcessTestBI/deliveryprocesses/finalize_documentation_9224FB35.html"];
+contentPage.processPage.elementUrls["_tkWPQOz3EemVzN-LgHwcjw"] = ["Data scientist","ProcessTestBI/deliveryprocesses/data_scientist_C3A559FC.html"];
+contentPage.processPage.elementUrls["_FnRAQOz5EemVzN-LgHwcjw"] = ["Monitor health and matrics","ProcessTestBI/deliveryprocesses/monitor_health_and_matrics_DDFA9C45.html"];
+contentPage.processPage.elementUrls["_43DWwOzwEemVzN-LgHwcjw"] = ["Create project charter","ProcessTestBI/deliveryprocesses/create_project_charter_DB568B1C.html"];
+contentPage.processPage.elementUrls["_ZqEx0Oz6EemVzN-LgHwcjw"] = ["Check in final artifacts","ProcessTestBI/deliveryprocesses/check_in_final_artifacts_F5A8731A.html"];
+contentPage.processPage.elementUrls["_wBj4kOz3EemVzN-LgHwcjw"] = ["Solution architect","ProcessTestBI/deliveryprocesses/solution_architect_FBE86747.html"];
+contentPage.processPage.elementUrls["_43D90ezwEemVzN-LgHwcjw"] = ["Project Charter","ProcessTestBI/deliveryprocesses/project_charter_2B8273FB.html"];
+contentPage.processPage.elementUrls["_aiV6wOzzEemVzN-LgHwcjw"] = ["Provision data infrastructure","ProcessTestBI/deliveryprocesses/provision_data_infrastructure_FD18D9D5.html"];
+contentPage.processPage.elementUrls["_WnDswOz6EemVzN-LgHwcjw"] = ["Checkpoint project","ProcessTestBI/deliveryprocesses/checkpoint_project_D4CC5A2F.html"];
+contentPage.processPage.elementUrls["_hYhIYOzzEemVzN-LgHwcjw"] = ["Design solution architecture","ProcessTestBI/deliveryprocesses/design_solution_architecture_651F707D.html"];
+contentPage.processPage.elementUrls["_wBjRgOz3EemVzN-LgHwcjw"] = ["Developp data pipeline","ProcessTestBI/deliveryprocesses/developp_data_pipeline_3985369.html"];
+contentPage.processPage.elementUrls["_HRH6Aez5EemVzN-LgHwcjw"] = ["Solution architect","ProcessTestBI/deliveryprocesses/solution_architect_4D21968C.html"];
+contentPage.processPage.elementUrls["_NeFWEOz6EemVzN-LgHwcjw"] = ["Acceptance","ProcessTestBI/deliveryprocesses/Acceptance_94DAFAA3.html"];
+contentPage.processPage.elementUrls["_hYhvcOzzEemVzN-LgHwcjw"] = ["Solution architect","ProcessTestBI/deliveryprocesses/solution_architect_39C574A0.html"];
+contentPage.processPage.elementUrls["_cbS8Qez6EemVzN-LgHwcjw"] = ["Project final report","ProcessTestBI/deliveryprocesses/project_final_report_790E2329.html"];
+contentPage.processPage.elementUrls["_ulcf4Oz3EemVzN-LgHwcjw"] = ["Model report","ProcessTestBI/deliveryprocesses/model_report_9A77DFA3.html"];
+contentPage.processPage.elementUrls["_ZqEx0ez6EemVzN-LgHwcjw"] = ["Data scientist","ProcessTestBI/deliveryprocesses/data_scientist_E4418730.html"];
+contentPage.processPage.elementUrls["_d_8qEOzzEemVzN-LgHwcjw"] = ["Data summary report","ProcessTestBI/deliveryprocesses/data_summary_report_EF7FB0F3.html"];
+contentPage.processPage.elementUrls["_WnET0Oz6EemVzN-LgHwcjw"] = ["Chief project","ProcessTestBI/deliveryprocesses/chief_project_5056A396.html"];
+contentPage.processPage.elementUrls["_a21f4Oz6EemVzN-LgHwcjw"] = ["Decomission compute assets","ProcessTestBI/deliveryprocesses/decomission_compute_assets_D540C32.html"];
+contentPage.processPage.elementUrls["_b2K6IOzzEemVzN-LgHwcjw"] = ["Provision compute assets","ProcessTestBI/deliveryprocesses/provision_compute_assets_8896FE16.html"];
